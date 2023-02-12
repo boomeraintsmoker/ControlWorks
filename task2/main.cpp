0d1cd7e19bdee897aa0d5b2aa50943e6f5f52374
Andrey <slawyan.pp@gmail.com>
2023-02-12 13:30:21 +0300

    Removed task0/ and task2/

--- task2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "task2.h"
-
-int main()
-{
-
-    Task2 tasker;
-    tasker.testMinMax(&tasker);
-    return 0;
-}
--- task2/task2.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK2_H
-#define TASK2_H
-class Task2 {
-public:
-    void testMinMax(Task2*);
-    int maxSearch(int* ar, int size);
-    int minSearch(int* ar, int size);
-};
-#endif
