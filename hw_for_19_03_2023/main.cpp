6df3cdae755d50078925aff56238af8a8e6a2eb3
Andrey <slawyan.pp@gmail.com>
2023-03-12 16:39:04 +0300

    start doing homework for 19.03.2023

--- hw_for_19_03_2023/main.cpp
+++ hw_for_19_03_2023/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+class Filo {
+	struct Element;
+	Element* first;
+public:
+	Filo(Element* _first);
+	~Filo();
+	void push(int value);
+	void pop();
+	void peek();
+	bool is_empty();
+	int size();
+};
+
+struct Filo::Element {
+	int value;
+	Element* el;	
+};
+
+Filo::Filo(Element* _first = nullptr): first(_first)  {
+	
+}
+
+Filo::~Filo() {
+	
+}
+
+void Filo::push(int value) {
+	if(first == nullptr) {
+		first->value = value;
+	}
+}
+
 int main() {
+	Filo stack;
+	stack.push(3);
 	return 0;
 }
