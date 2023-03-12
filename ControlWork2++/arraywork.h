d92500aee782e24099fb4f610f68a395bf5af6d0
metaller <117290144+boomeraintsmoker@users.noreply.github.com>
2023-03-12 13:50:09 +0300

    Delete ControlWork2++ directory

--- ControlWork2++/arraywork.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARRAYWORK_H
-#define ARRAYWORK_H
-void randomAr(int* ar, int size);
-double sortTime(void(*sort)(int*, int), int* ar, int size);
-double addSortTimeToFile(FILE* f, int i, double time);
-#endif // ARRAYWORK_H
